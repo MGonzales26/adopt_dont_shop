4592ca64e9280b4e5539189885bc43cc9ac90823
Megan Campbell <megan.campbell26@yahoo.com>
2020-12-12 14:52:28 -0700

    Add presence validation

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,8 @@
 class Application < ApplicationRecord
   has_many :pets
+  validates_presence_of :name, 
+                        :street_address, 
+                        :city, 
+                        :state, 
+                        :zip_code
 end
