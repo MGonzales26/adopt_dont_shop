0133e56a79cbb553c4e8b1fb263d460e131d7941
Megan Campbell <megan.campbell26@yahoo.com>
2020-12-12 14:33:24 -0700

    Add new and create actions

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -6,4 +6,16 @@ class ApplicationsController < ApplicationController
 
   def new
   end
+
+  def create
+    @application = Application.create!({
+      name: params[:name],
+      street_address: params[:street_address],
+      city: params[:city],
+      state: params[:state],
+      zip_code: params[:zip_code],
+      status: "In Progress"
+      })
+      redirect_to "/applications/#{@application.id}"
+  end
 end
